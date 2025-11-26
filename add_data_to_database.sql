185816538c593c9dc0a46700285c41693a1a0769
Collins Kibet <kollcibe05@gmail.com>
2025-11-26 18:18:35 +0300

    many-to-many join tables

--- add_data_to_database.sql
+++ add_data_to_database.sql
@@ -1,5 +1,13 @@
-INSERT INTO cats (name, age, breed)
-VALUES ("Maru", 3, "Scottish Fold");
+-- INSERT INTO cats (name, age, breed)
+-- VALUES ("Maru", 3, "Scottish Fold");
 
-INSERT INTO cats (name, age, breed)
-VALUES ("Hana", 1, "Tabby");
+-- INSERT INTO cats (name, age, breed)
+-- VALUES ("Hana", 1, "Tabby");
+
+INSERT INTO owners (name) VALUES ("mugumogu");
+INSERT INTO owners (name) VALUES ("Sophie");
+INSERT INTO owners (name) VALUES ("Penny");
+INSERT INTO cats (name, age, breed) VALUES ("Maru", 3, "Scottish Fold");
+INSERT INTO cats (name, age, breed) VALUES ("Hana", 1, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Nona", 4, "Tortoiseshell");
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 2, "perma-kitten");
